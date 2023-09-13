ac89c5379b2af5b04458288d4ddc73fc546dbc0d
JONATAN140502 <jmayangamu@unprg.edu.pe>
2023-09-13 14:51:09 -0500

    doskpi pipi

---
+++ database/migrations/2023_09_13_192547_create_kpiregistro_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKpiregistroTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kpiregistro_students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('id_student');
+            $table->foreign('id_student')->references('id')->on('students');
+            $table->dateTime('last_access');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kpiregistro');
+    }
+}
